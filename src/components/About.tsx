42ceed0738a698054d1c32d338152b01534e3b4c
RuanParreira <ruan.parreira@edu.uniube.br>
2025-06-04 22:35:16 -0300

    Atualização

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,17 +4,15 @@ export default function About() {
   return (
     <section
       id="about"
-      className="relative flex h-[calc(100vh-5rem)] w-full scroll-mt-[5rem] items-center justify-between space-x-[24rem] bg-[#0f0f0f]"
+      className="relative flex h-[calc(100vh-5rem)] w-full scroll-mt-[5rem] items-center justify-between space-x-20 bg-[#0f0f0f] px-[15%]"
     >
-      <div className="z-0">
-        <img
-          src="./about2.png"
-          alt="fotoSobre"
-          className="absolute bottom-0 left-0 z-0 h-[40rem] mask-t-from-80% mask-r-from-85% mask-l-from-10%"
-        />
-      </div>
+      <img
+        src="./cartoon.png"
+        alt="fotoSobre"
+        className="h-[30rem] w-[50rem] mask-t-from-98% mask-r-from-98% mask-b-from-98% mask-l-from-98%"
+      />
 
-      <div className="z-10 flex w-[full] min-w-0 flex-col justify-center px-[15%]">
+      <div className="z-10 flex w-[full] min-w-0 flex-col justify-center">
         <h3 className="text-4xl">SOBRE MIM</h3>
         <span className="mt-5 mb-4 block h-1 w-24 bg-blue-500"></span>
         <p className="text-justify text-lg">
